007835bbe94061b8e740db2ec7bcabb7260ba71f
Andrew Fisher <AndrewfFishersb@gmail.com>
2016-09-16 09:55:34 -0700

    creates a definition class and corresponding tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,3 +3,29 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+
+
+public class App{
+
+  public static void main(String[] args) {
+    //public thing goes here if need be
+    String layout = "templates/layout.vtl";
+
+    get("/",(request,response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template","templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      String theWord = request.queryParams("word");
+      Word aWord = new Word(theWord);
+      
+
+
+
+
+    }, new VelocityTemplateEngine());
+  }
+}
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -4,45 +4,52 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class DefinitionTest {
-    @After
+
+  @After
     public void tearDown(){
-      Word.clear();
+      Definition.clear();
+  }
+  @Test
+  public void definition_createDefinition_true(){
+    Definition definitionTest = new Definition("a glial cell similar to an astrocyte but with fewer protuberances, concerned with the production of myelin in the central nervous system.");
+    assertTrue(definitionTest instanceof Definition);
+  }
+
+  @Test
+  public void getDefinition_RetrievesTheDefinition_String(){
+    Definition definitionTest = new Definition("a glial cell similar to an astrocyte but with fewer protuberances, concerned with the production of myelin in the central nervous system.");
+    String expectedOutcome = "a glial cell similar to an astrocyte but with fewer protuberances, concerned with the production of myelin in the central nervous system.";
+    assertEquals(expectedOutcome, definitionTest.getDefinition());
+  }
+
+  @Test
+  public void all_ShowsAllWords_List(){
+    Definition definitionTest = new Definition("speed up the progress of; facilitate");
+    Definition testDefinition = new Definition("having or showing arrogant superiority to");
+    assertTrue(Definition.all().contains(definitionTest));
+    assertTrue(Definition.all().contains(testDefinition));
+  }
+
+  @Test
+  public void clear_ClearsTheArray_0(){
+    Definition definitionTest = new Definition("speed up the progress of; facilitate");
+    Definition.clear();
+    assertEquals(0,Definition.all().size());
+  }
+
+  @Test
+  public void getId_GetsIdOfCurrentDefinition_1(){
+    Definition definitionTest = new Definition("speed up the progress of; facilitate");
+    assertEquals(1,definitionTest.getId());
+  }
+
+  @Test
+  public void find_FindsASpecificWord_2(){
+    Definition definitionTest = new Definition("speed up the progress of; facilitate");
+    Definition testDefinition = new Definition("having or showing arrogant superiority to");
+    assertEquals(testDefinition, Definition.find(testDefinition.getId()));
   }
 
 
 
-
-
-  
 }
-
-
-
-
-
-
-// public Definition(String definition){
-//   mDefinition = definition;
-//   instances.add(this);
-//   mId = instances.size();
-// }
-//
-// public String getDefinition(){
-//   return mDefinition;
-// }
-//
-// public static List<Definition> all(){
-//   return instances;
-// }
-//
-// public static void clear(){
-//   instances.clear();
-// }
-//
-// public int getId(){
-//   return mId;
-// }
-//
-// public static Definition find(int id){
-//   return instances.get(id-1);
-// }
